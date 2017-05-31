1f0a6ebf4530aa65df698a5532719e782b5cefc9
Yuriy Yakym <ziqueq@gmail.com>
2017-06-01 01:40:06 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import * as Rx from 'rxjs';
+import { combineObjectObservables, groupParams } from './utils';
 import createLifecycle from './lifecycle-proxy';
 import { LIFECYCLE_METHOD, LIFECYCLE_METHODS } from './constants.js';
 
@@ -7,22 +8,9 @@ import { LIFECYCLE_METHOD, LIFECYCLE_METHODS } from './constants.js';
 export const reax = (arg) => (Component) => {
   const lifecycle$ = new Rx.Subject();
   const props$ = new Rx.Subject();
-
-  const streams = getParams(arg, lifecycle$, props$);
-
-  const streamNames = Object.keys(streams);
-  const streams$ = Object.values(streams);
-
-  const combinedStreamsValues$ = Rx.Observable.combineLatest(
-    ...streams$,
-    (...values) => streamNames.reduce(
-      (result, streamName, index) => {
-        result[streamName] = values[index];
-        return result;
-      },
-      {}
-    )
-  );
+  const params = getParams(arg, lifecycle$, props$);
+  const { observables, properties } = groupParams(params);
+  const combinedStreamsValues$ = combineObjectObservables(observables);
 
   return class extends PureComponent {
     constructor(props) {
@@ -44,7 +32,7 @@ export const reax = (arg) => (Component) => {
     }
 
     render() {
-      return <Component {...this.state} {...this.props} />
+      return <Component {...properties} {...this.state} {...this.props} />
     }
   };
 }
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,33 @@
+import * as Rx from 'rxjs';
+
 export const replicateFunction = (subject, fn) =>
   (...args) => subject.next(fn(...args));
 
 export const replicateEvent = (subject, fn) =>
   (event) => subject.next(fn ? fn(event) : event);
+
+export const groupParams = (obj) => Object.keys(obj).reduce(
+  (result, key) => {
+    const group = obj[key] instanceof Rx.Observable
+      ? 'observables'
+      : 'properties';
+    result[group][key] = obj[key];
+    return result;
+  },
+  { observables: {}, properties: {} }
+);
+
+export const combineObjectObservables = (obj) => {
+  const observablesNames = Object.keys(obj);
+  const observables$ = Object.values(obj);
+  return Rx.Observable.combineLatest(
+    ...observables$,
+    (...values) => observablesNames.reduce(
+      (result, streamName, index) => {
+        result[streamName] = values[index];
+        return result;
+      },
+      {}
+    )
+  );
+};
